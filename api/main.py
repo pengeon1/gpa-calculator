319f0aecf3fb70389d9c6c15e939ff8e99c82040
Piyush Mishra <piyushmishra0207@gmail.com>
2024-12-10 16:36:02 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,26 +1,9 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse, JSONResponse
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
-@app.get("/", response_class=HTMLResponse)
-async def get_index():
-    with open("static/index.html", "r") as file:
-        content = file.read()
-    return HTMLResponse(content=content)
-
-@app.post("/main")
+@app.post("/calculate")
 async def calculate_gpa(data: Request):
     body = await data.json()
     courses = body.get("courses", [])
@@ -33,6 +16,6 @@ async def calculate_gpa(data: Request):
         grade = float(course.get("grade", 0))
         total_credits += credit
         weighted_score += credit * grade
-        print(weight_score,total_credits)
+
     gpa = weighted_score / total_credits if total_credits > 0 else 0
-    return JSONResponse({"gpa": round(gpa, 4)})
+    return JSONResponse({"gpa": round(gpa, 2)})
